e8f3ed1b380ef3bcb325f0975730012eceb51093
Connor Callison <cjc736@humboldt.edu>
2017-10-25 11:32:25 -0700

    Fixed script import issues, and placed background image under /public

--- PlusOne/client/main.js
+++ PlusOne/client/main.js
@@ -4,6 +4,13 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import './main.html';
 import './main.css';
 import './creative.js';
+import './vendor/jquery/jquery.min.js';
+import './vendor/bootstrap/js/bootstrap.bundle.min.js';
+import './vendor/jquery-easing/jquery.easing.min.js';
+import './vendor/scrollreveal/scrollreveal.min.js';
+import './vendor/magnific-popup/jquery.magnific-popup.min.js';
+import './vendor/bootstrap/js/bootstrap.bundle.min.js';
+import './vendor/bootstrap/js/bootstrap.bundle.min.js';
 
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
