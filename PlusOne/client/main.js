ca9e61839dfc5cbf727f563b9d5f20048826b73b
jbmcgreevy <31285983+jbmcgreevy@users.noreply.github.com>
2017-10-25 10:18:01 -0700

    Update main.js

--- PlusOne/client/main.js
+++ PlusOne/client/main.js
@@ -2,7 +2,7 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
-
+import './main.css';
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
