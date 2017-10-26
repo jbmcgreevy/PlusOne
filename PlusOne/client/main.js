e307ca817e1cc0fcae76d7b1279cb86fb3a37d2a
Connor Callison <cjc736@humboldt.edu>
2017-10-26 12:47:25 -0700

    Slight cleanup

--- PlusOne/client/main.js
+++ PlusOne/client/main.js
@@ -2,15 +2,9 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
-// import './main.css';
+import './main.css';
 // import './creative.js';
-// import './vendor/jquery/jquery.min.js';
-// import './vendor/bootstrap/js/bootstrap.bundle.min.js';
-// import './vendor/jquery-easing/jquery.easing.min.js';
-// import './vendor/scrollreveal/scrollreveal.min.js';
-// import './vendor/magnific-popup/jquery.magnific-popup.min.js';
-// import './vendor/bootstrap/js/bootstrap.bundle.min.js';
-// import './vendor/bootstrap/js/bootstrap.bundle.min.js';
+
 
 Router.route('/home', function () {
   this.render('Home');
