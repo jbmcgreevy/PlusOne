d2b45c2847387257c6e2488a339d6fa152623db6
Connor Callison <cjc736@humboldt.edu>
2017-10-26 12:16:38 -0700

    FINALLY FIXED THE ROUTING ISSUE (CAPS INTENDED)

--- PlusOne/client/main.js
+++ PlusOne/client/main.js
@@ -2,13 +2,18 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
-import './main.css';
-import './creative.js';
-import './vendor/jquery/jquery.min.js';
-import './vendor/bootstrap/js/bootstrap.bundle.min.js';
-import './vendor/jquery-easing/jquery.easing.min.js';
-import './vendor/scrollreveal/scrollreveal.min.js';
-import './vendor/magnific-popup/jquery.magnific-popup.min.js';
-import './vendor/bootstrap/js/bootstrap.bundle.min.js';
-import './vendor/bootstrap/js/bootstrap.bundle.min.js';
+// import './main.css';
+// import './creative.js';
+// import './vendor/jquery/jquery.min.js';
+// import './vendor/bootstrap/js/bootstrap.bundle.min.js';
+// import './vendor/jquery-easing/jquery.easing.min.js';
+// import './vendor/scrollreveal/scrollreveal.min.js';
+// import './vendor/magnific-popup/jquery.magnific-popup.min.js';
+// import './vendor/bootstrap/js/bootstrap.bundle.min.js';
+// import './vendor/bootstrap/js/bootstrap.bundle.min.js';
 
+Router.route('/home', function () {
+  this.render('Home');
+}, {
+  name: 'home'
+});
