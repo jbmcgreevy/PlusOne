6ff220b0bd4d56d74702dca12f759683b2b5a06a
Connor Callison <cjc736@humboldt.edu>
2017-10-26 10:39:35 -0700

    added iron router

--- PlusOne/client/main.js
+++ PlusOne/client/main.js
@@ -12,20 +12,3 @@ import './vendor/magnific-popup/jquery.magnific-popup.min.js';
 import './vendor/bootstrap/js/bootstrap.bundle.min.js';
 import './vendor/bootstrap/js/bootstrap.bundle.min.js';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
