27062e816cf91073dcef5a294a8b128dd08d1fc3
theJosey <kevinjosey13@gmail.com>
2017-11-28 09:10:42 -0800

    Create matchmakingBasic.py

---
+++ matchmaking/matchmakingBasic.py
@@ -0,0 +1,8 @@
+# Pseuodo code for now !!!!!!!!!!!!!!!!!!!!!
+# Created by Kevin Josey
+# Last Modified 16 November 2017
+
+# Ideally we use que so that users will only match with each other by game but 
+#	this can still be general algorithm
+
+# This version will match people within game choice and that's it, no other inputs will be evaluated.
